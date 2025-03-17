a010a38c3db7352c9b2fa93e1e2578c04943c8e2
Lao04 <122139045+MichaelAngelLu@users.noreply.github.com>
2025-03-17 23:03:05 +0800

    Revert "Pre-work with manage jobs (admin)"
    This reverts commit 59082e8c703dec5f361ef74d8b6a372c0184cca3.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,10 +8,6 @@ import JobListingsView from '@/views/JobListingsView.vue'
 import LoginView from '@/views/LoginView.vue'
 import AdminDashboardView from '@/views/AdminDashboardView.vue'
 import ApplicantDashboardView from '@/views/ApplicantDashboardView.vue'
-import ManageJobs from '@/views/ManageJobs.vue'
-import AdminApplications from '@/views/AdminApplications.vue'
-import ManageUsers from '@/views/ManageUsers.vue'
-import AdminSettings from '@/views/AdminSettings.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
@@ -20,12 +16,7 @@ const routes = [
   { path: '/jobs', name: 'JobListings', component: JobListingsView },
   { path: '/login', name: 'Login', component: LoginView },
   { path: '/admin', name: 'AdminDashboard', component: AdminDashboardView },
-  { path: '/applicant', name: 'ApplicantDashboard', component: ApplicantDashboardView },
-
-  { path: '/admin/jobs', name: 'ManageJobs', component: ManageJobs },
-  { path: '/admin/applications', name: 'Applications', component: AdminApplications },
-  { path: '/admin/users', name: 'ManageUsers', component: ManageUsers },
-  { path: '/admin/settings', name: 'AdminSettings', component: AdminSettings }
+  { path: '/applicant', name: 'ApplicantDashboard', component: ApplicantDashboardView }
 ]
 
 const router = createRouter({
