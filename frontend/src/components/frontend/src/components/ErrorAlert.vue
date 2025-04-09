60349623ff4ca407aae5200905141ffdf9fb04b7
Neina  Burce <122615096+Neina26@users.noreply.github.com>
2025-04-09 22:02:26 +0700

    Add reusable ErrorAlert component

---
+++ frontend/src/components/frontend/src/components/ErrorAlert.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-if="message" class="bg-red-100 text-red-700 p-3 rounded mb-3">
+      {{ message }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['message'],
+  };
+  </script>
+  
